27241ef043f527b28218ab350754f022be5986de
samtarlo3011 <samuelt136@nycstudents.net>
2026-01-30 10:17:10 -0500

    Update 1_29.py

--- 1_29.py
+++ 1_29.py
@@ -1,3 +1,17 @@
-values = [1,2.23,5,7,2,30,15]
-print(values[0])
-print(values[4])
+""" x = "this is a thing"
+y= x.split( )
+z = y[1]
+print(y)
+print(z) """
+sentence=input("write sentence here:")
+y=sentence.split( )
+def wordcount():
+    x=0
+    z=0
+    for i in range(60):
+        print(y[x])
+        x=x+1
+        z=z+1
+    print(z)
+
+wordcount()
