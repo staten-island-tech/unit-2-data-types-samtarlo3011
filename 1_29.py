29bbdba4b445f0b65ef55fd612e557564b174aec
samtarlo3011 <samuelt136@nycstudents.net>
2026-02-02 10:17:16 -0500

    Update 1_29.py

--- 1_29.py
+++ 1_29.py
@@ -3,15 +3,41 @@ y= x.split( )
 z = y[1]
 print(y)
 print(z) """
-sentence=input("write sentence here:")
-y=sentence.split( )
-def wordcount():
-    x=0
-    z=0
-    for i in range(60):
-        print(y[x])
-        x=x+1
-        z=z+1
-    print(z)
 
-wordcount()
+def wordcount():
+    sentence=input("write sentence here:")
+    y=sentence.split( )
+    x=0
+    for i in (y):
+        x=x+1
+    print(x)
+
+def odd_or_even():
+    number=input("enter a number: ")
+    odd_even=(int(number) % 2)
+    if (odd_even) == 1:
+        print("odd")
+    else:
+        print("even")
+def tip_value():
+    satisfaction=input("was your service: Bad, Okay, Good, Great?")
+    bill=input("How Much Was Your Bill?")
+    int(bill)
+    tip=0
+    if satisfaction == Bad:
+        tip=tip+0
+    elif satisfaction == Okay:
+        tip = tip+15
+    elif satisfaction == Good:
+        tip = tip + 20
+    else:
+        tip=tip+25
+    int(tip)
+    tip=(tip/100)
+    tip=(tip+1)
+    total=(bill*tip)
+    print(f"bill={bill}")
+    print(f"tip={tip}")
+    print(f"total={total}")
+
+tip_value()
